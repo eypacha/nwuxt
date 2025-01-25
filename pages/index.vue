77f49dc50d2fcb5c28cfdc9ea75eb161d7528647
Ey Pacha! <pachaguionbajo@gmail.com>
2025-01-25 16:16:27 -0300

    fix: update greeting text in index.vue for clarity

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center justify-center flex-1 h-full p-6 text-center">
     <section>
-      <h1 class="mb-5 text-4xl font-bold text-primary">Hello my nuwxt web!</h1>
+      <h1 class="mb-5 text-4xl font-bold text-primary">Hello world!</h1>
     </section>
   </div>
 </template>
