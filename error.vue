53b756e5c0c8a2574154d3d1a8eee5d729dc77be
Ey Pacha! <pachaguionbajo@gmail.com>
2025-01-25 15:53:47 -0300

    feat: add error handling component with customizable error messages and navigation

---
+++ error.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps({
+  error: {
+    type: Object,
+    required: true,
+  },
+  default: () => ({ statusCode: 500, message: "Internal Server Error" }),
+});
+
+const goToHome = () => clearError({ redirect: '/' });
+</script>
+
+<template>
+  <div class="container p-6 text-center">
+    <h1 class="font-bold text-9xl text-destructive">{{ props.error.statusCode }}</h1>
+    <h2 class="mb-4 text-2xl">{{ props.error.message }}</h2>
+    <Button @click="goToHome">Go to Home</Button>
+  </div>
+</template>
