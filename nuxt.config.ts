9a5821afa790b0bea9d73e0f06bac6e2fcc34a9e
Ey Pacha! <pachaguionbajo@gmail.com>
2025-01-25 14:31:47 -0300

    add utility functions and components for Tailwind CSS integration; update configuration files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,28 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode']
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+    'shadcn-nuxt'
+  ],
+  tailwindcss: {
+    configPath: 'tailwind.config.js',
+    cssPath: 'assets/scss/main.scss',
+    exposeConfig: false,
+    viewer: false,
+  },
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui'
+  },
+  colorMode: {
+    preference: 'system',
+    fallback: 'light',
+    classPrefix: '',
+    classSuffix: '',
+    storage: 'localStorage',
+    storageKey: 'nuxt-color-mode',
+  }
+
 })
