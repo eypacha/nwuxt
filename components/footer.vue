03611056b7492692f42a4978be020da1e367e9c3
Ey Pacha! <pachaguionbajo@gmail.com>
2025-01-25 16:17:10 -0300

    feat: update header and footer components to use runtime configuration for app title

--- components/footer.vue
+++ components/footer.vue
@@ -1,12 +1,18 @@
 <template>
-  <footer class="flex items-center justify-between p-4 bg-secondary">
-    <span>&copy; 2025 Nwuxt</span>
+  <footer class="flex items-center justify-between p-4 text-sm bg-secondary">
+    <span>&copy; {{ currentYear }} {{ appTitle }}</span>
     <a
       href="https://github.com/eypacha/nuwxt"
       target="_blank"
-      class="text-3xl hover:text-primary"
+      class="text-2xl"
       aria-label="Visit Nwuxt GitHub repository"
       ><Icon name="uil:github" aria-hidden="true"
     /></a>
   </footer>
 </template>
+
+<script lang="ts" setup>
+const config = useRuntimeConfig();
+const appTitle = config.public.appTitle;
+const currentYear = new Date().getFullYear();
+</script>
--- components/header.vue
+++ components/header.vue
@@ -1,6 +1,11 @@
 <template>
-  <header class="flex items-center justify-between p-4 bg-secondary">
-      <div>BrandLogo</div>
-       <ColorModeSelector/>
+  <header class="flex items-center justify-between p-3 bg-secondary">
+    <div>{{ appTitle }}</div>
+    <ColorModeSelector/>
   </header>
 </template>
+
+<script lang="ts" setup>
+const config = useRuntimeConfig();
+const appTitle = config.public.appTitle;
+</script>
