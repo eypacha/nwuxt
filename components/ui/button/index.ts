85ff941ea027692259b140512de168faa6f6a901
Ey Pacha! <pachaguionbajo@gmail.com>
2025-01-25 15:53:18 -0300

    fix: update default button variant background color to primary

--- components/ui/button/index.ts
+++ components/ui/button/index.ts
@@ -8,7 +8,7 @@ export const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          'bg-red text-primary-foreground shadow hover:bg-primary/90',
+          'bg-primary text-primary-foreground shadow hover:bg-primary/90',
         destructive:
           'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
         outline:
